ebf13bf87ee6fc71fab2ff430554b1bf71fe95f3
Patricio Mosse <patricio.mosse.software@gmail.com>
2025-08-29 17:15:12 +0200

    fix ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ntt-service',
-      script: './dist/src/ntt-service.js',
+      script: './dist/index.js',
       instances: 1,
       autorestart: true,
       watch: false,
